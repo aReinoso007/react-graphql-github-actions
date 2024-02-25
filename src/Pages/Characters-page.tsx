29bf6b5e50a19905297112662e0ee85a67057593
Alex Reinoso <ralexjessiel@gmail.com>
2024-02-25 17:16:17 -0500

    test

--- src/Pages/Characters-page.tsx
+++ src/Pages/Characters-page.tsx
@@ -10,7 +10,7 @@ const CharactersPage: React.FC = () => {
 
   return (
     <>
-      <h1>Characters page</h1>
+      <h1>Characters page cambio pepa para prueba</h1>
       <Row>
         {data.characters.results.map((character: any, index: any) => {
           return (
